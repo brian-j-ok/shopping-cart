94b54a5b932fa16db0ebb3ff70dd9a3d80cc85b6
brianjrok <brianjrok@gmail.com>
2022-07-10 16:20:28 -0500

    temp css changes

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import "../styles/App.css";
 
 const Shop = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  const fetchProducts = async () => {
+    const data = await fetch('https://fakestoreapi.com/products');
+    const products = await data.json();
+    console.log(products);
+    setProducts(products);
+  }
+
   return (
-    <div>
-      <h1>Shop</h1>
+    <div className="product-container">
+      {products.map(product => (
+        <div className="product">
+          <img src={product.image} alt={product.title} />
+          <h3>{product.title}</h3>
+          <h4>Price: {product.price}</h4>
+          <p>{product.description}</p>
+          <div>
+            <input type="number" min="0" max="10" defaultValue="0" />
+            <button type="submit">Add</button>
+          </div>
+        </div>
+      ))}
     </div>
   )
 }
