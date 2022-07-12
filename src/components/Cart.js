c05d6a0f44aba91d2413035ca12efd7c07d356f8
brianjrok <brianjrok@gmail.com>
2022-07-11 20:34:22 -0500

    logic to render quantity and remove items from cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Cart = ({cart, setCart}) => {
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    let cartTotal = 0;
+    cart.forEach(product => {
+      cartTotal += (product.price * product.quantity);
+    })
+    setTotal(cartTotal);
+  }, [cart]);
+
+  const removeItem = (id) => {
+    setCart(cart =>
+      cart.filter(product => {
+        return product.id !== id;
+      })
+    )
+  }
+
   return (
     <div>
       <ul>
@@ -12,8 +30,12 @@ const Cart = ({cart, setCart}) => {
               <h4>$ {product.price}</h4>
               <h4>Quantity: {product.quantity}</h4>
             </div>
+            <div>
+              <button onClick={() => removeItem(product.id)}>Remove</button>
+            </div>
           </li>
         ))}
+        <h1>Cart Total: {total}</h1>
       </ul>
     </div>
   )
